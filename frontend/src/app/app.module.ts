0ddd51d46ec4541e4becca70b63687bf0084faa4
Jonathan.Martin <jonathan.martin@evidentscientific.com>
2023-12-13 11:09:15 -0500

    PR

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeviceListComponent } from './devices/device-list/device-list.component';
+import { DeviceComponent } from './devices/device/device.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeviceListComponent,
+    DeviceComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
